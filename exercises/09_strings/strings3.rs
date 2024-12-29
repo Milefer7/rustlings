02c11189357a86053d0f876dc384acf02af15238
Jasper Jiang <williammilefer@gmail.com>
2024-12-29 11:17:20 +0800

    task: to 46
    Signed-off-by: Jasper Jiang <williammilefer@gmail.com>

--- exercises/09_strings/strings3.rs
+++ exercises/09_strings/strings3.rs
@@ -1,13 +1,21 @@
 fn trim_me(input: &str) -> &str {
     // TODO: Remove whitespace from both ends of a string.
+    input.trim()
 }
 
 fn compose_me(input: &str) -> String {
     // TODO: Add " world!" to the string! There are multiple ways to do this.
+    // let mut tem = input.to_string();
+    // tem.push_str(" world!");
+    // tem
+    format!("{input} world!")
 }
 
 fn replace_me(input: &str) -> String {
     // TODO: Replace "cars" in the string with "balloons".
+    // let tem = input.to_string();
+    // tem.replace("cars", "balloons")
+    input.replace("cars", "balloons")
 }
 
 fn main() {
